ddaaf6151b17dbe41f6188a43a76d4dbcfd88a23
Ruvin Rodrigues <ruvin27@gmail.com>
2022-12-05 18:01:21 -0600

    req 1

--- index.py
+++ index.py
@@ -18,7 +18,17 @@ class sql_objects:
         conn.execute("SELECT TABLE_NAME FROM all_tables WHERE OWNER = 'AXP2009' ORDER BY TABLE_NAME")
         print("\n")
         for row in conn:
-            print (row[0])   
+            print (row[0])
+
+    def show_books(self, conn):
+        conn.execute("SELECT * FROM AXP2009.FALL22_S001_13_BOOKS")
+        print("\n")
+        for row in conn:
+            print(row[3],end=" - ")
+            print(row[4])
+            print("Category:"+ row[5])
+            print(row[2])  
+            print("\n")
 
 
 p1 = sql_objects()
@@ -26,12 +36,15 @@ conn = p1.connect()
 while True:
     print("\nMAIN MENU")  
     print("1. SHOW RELATIONS")  
-    print("2. MODIFY RELATIONS")  
+    print("2. SHOW ALL BOOKS IN THE LIBRARY")
     print("3. RUN QUERY")  
+    print("4. MODIFY RELATIONS")  
     choice1 = int(input("Enter the Choice:"))
 
     if choice1 == 1:
         p1.get_table_names(conn)
+    elif choice1 == 2:
+        p1.show_books(conn)
     else:
         p1.close_connection(conn)
         break
