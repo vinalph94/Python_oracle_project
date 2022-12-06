bee136515cad7f7e03ef49c22f9d71adfb145124
P.S.S.ABISHEK <pssabishek18@gmail.com>
2022-12-05 18:03:57 -0600

    fixed errors

--- index.py
+++ index.py
@@ -19,7 +19,19 @@ class sql_objects:
         conn.execute("SELECT TABLE_NAME FROM all_tables WHERE OWNER = 'AXP2009' ORDER BY TABLE_NAME")
         print("\n")
         for row in conn:
-            print (row[0])   
+            print (row[0])
+
+    def get_table_data(self, conn, table_name):
+        try:
+            conn.execute("SELECT * FROM AXP2009." + table_name + " FETCH FIRST 25 ROWS ONLY")
+            rows = conn.fetchall()
+            print(tabulate(rows, showindex=False, tablefmt='psql'))
+        except cx_Oracle.DatabaseError as er:
+            print('There is error in the Oracle database:', er)
+ 
+        except Exception as er:
+            print('Error:', er)
+
 
     def getMostBorrowedBooks(self,conn):
         #RETURN the Name, ID and Genre OF the book which was borrowed n or more times IN the LAST quarter OF 2022
@@ -41,7 +53,9 @@ class sql_objects:
             fee = input("\nEnter the value for total fee :")
             conn.execute("SELECT Fname, Lname, fss.student_ID, sum(LATE_FEE) AS Total_Penalty FROM AXP2009.FALL22_S001_13_STUDENTS fss, AXP2009.FALL22_S001_13_BORROW_RECORD fsbr WHERE fss.student_ID = fsbr.STUDENT_ID  GROUP BY Fname, Lname, fss.student_ID HAVING sum(LATE_FEE) >= :amount ORDER BY sum(LATE_FEE) DESC", {'amount': fee})
             rows = conn.fetchall()
-            print(tabulate(rows, showindex=False, headers=['FNAME', 'LNAME', 'STUDENT_ID', 'TOTAL_PENALTY'], tablefmt='psql'))
+            for rows in conn:
+                print (rows[0])
+            # print(tabulate(rows, showindex=False, headers=['FNAME', 'LNAME', 'STUDENT_ID', 'TOTAL_PENALTY'], tablefmt='psql'))
            
         except cx_Oracle.DatabaseError as er:
             print('There is error in the Oracle database:', er)
@@ -55,25 +69,37 @@ class sql_objects:
             conn.execute("select * from (SELECT Name, COUNT(Name) AS times FROM AXP2009.FALL22_S001_13_PUBLISHER fsp, AXP2009.FALL22_S001_13_PUBLISHED_BY fspb, AXP2009.FALL22_S001_13_BORROW_RECORD fsbr WHERE fsp.PUBLISHER_ID = fspb.PUBLISHER_ID AND fspb.BOOK_ID = fsbr.BOOK_ID GROUP BY NAME) where times=(select Max(times) from (SELECT Name, COUNT(Name) AS times FROM AXP2009.FALL22_S001_13_PUBLISHER fsp, AXP2009.FALL22_S001_13_PUBLISHED_BY fspb, AXP2009.FALL22_S001_13_BORROW_RECORD fsbr WHERE fsp.PUBLISHER_ID = fspb.PUBLISHER_ID AND fspb.BOOK_ID = fsbr.BOOK_ID GROUP BY NAME))")
             rows = conn.fetchall()
             print(tabulate(rows, showindex=False, headers=['NAME', 'TIMES'], tablefmt='psql'))
+        except cx_Oracle.DatabaseError as er:
+            print('There is error in the Oracle database:', er)
+ 
+        except Exception as er:
+            print('Error:', er)
 
-    def pl.getAverageLateFee(self,conn):
-
+    def getAverageLateFee(self,conn):
         try:
             conn.execute("SELECT DATE_OF_ISSUE, round(AVG(late_fee) OVER (ORDER BY DATE_OF_ISSUE ROWS BETWEEN 1 PRECEDING AND 1 FOLLOWING),2) as AVG_LATE_FEE from AXP2009.FALL22_S001_13_BORROW_RECORD fsbr") 
             rows = conn.fetchall()
             print(tabulate(rows, showindex=False, headers=['DATE_OF_ISSUE'],tablefmt='psql'))
+        except cx_Oracle.DatabaseError as er:
+            print('There is error in the Oracle database:', er)
+ 
+        except Exception as er:
+            print('Error:', er)
 
-    def pl.getTotalLateFee(self,conn):
-
+    def getTotalLateFee(self,conn):
         try:
             conn.execute("SELECT fsbr.BOOK_ID, fsbr.DATE_OF_ISSUE, SUM(fsbr.LATE_FEE) FROM FALL22_S001_13_BORROW_RECORD fsbr INNER JOIN FALL22_S001_13_BOOKS fsb ON fsbr.BOOK_ID = fsb.BOOK_ID GROUP BY fsbr.BOOK_ID, ROLLUP(fsbr.DATE_OF_ISSUE)") 
             rows = conn.fetchall()
             print(tabulate(rows, showindex=False, headers=['BOOK_ID','DATE_OF_ISSUE','LATE_FEE'],tablefmt='psql'))  
+        except cx_Oracle.DatabaseError as er:
+            print('There is error in the Oracle database:', er)
+ 
+        except Exception as er:
+            print('Error:', er)
 
-    def pl.getCountForQuarter(self,conn):
-
+    def getCountForQuarter(self,conn):
         try:
-            conn.execute("SELECT fss.FNAME, TO_CHAR(fsb.REGISTERED_BOOK_ON, 'Q') AS "QUARTER", fsb.GENRE, COUNT(*) FROM FALL22_S001_13_BOOKS fsb INNER JOIN FALL22_S001_13_STAFF fss ON fss.STAFF_ID = fsb.STAFF_ID GROUP BY TO_CHAR(fsb.REGISTERED_BOOK_ON, 'Q'), fss.FNAME, ROLLUP(fsb.GENRE) FETCH NEXT 10 ROWS ONLY") 
+            conn.execute("SELECT fss.FNAME, TO_CHAR(fsb.REGISTERED_BOOK_ON, 'Q') AS 'QUARTER', fsb.GENRE, COUNT(*) FROM FALL22_S001_13_BOOKS fsb INNER JOIN FALL22_S001_13_STAFF fss ON fss.STAFF_ID = fsb.STAFF_ID GROUP BY TO_CHAR(fsb.REGISTERED_BOOK_ON, 'Q'), fss.FNAME, ROLLUP(fsb.GENRE) FETCH NEXT 10 ROWS ONLY") 
             rows = conn.fetchall()
             print(tabulate(rows, showindex=False, headers=['FNAME','GENRE'],tablefmt='psql'))                          
            
@@ -95,9 +121,12 @@ while True:
 
     if choice1 == 1:
         p1.get_table_names(conn)
+        tbl_name = input('Enter the table name: ')
+        p1.get_table_data(conn=conn, table_name=tbl_name)
+        
 
     elif choice1 == 3:
-        print("\MENU FOR BUSINESS GOALS")  
+        print("\nMENU FOR BUSINESS GOALS")  
         print("1. Show the Name, ID and Genre OF the book which was borrowed N or more times IN the LAST quarter OF 2022")  
         print("2. Show the name AND ID OF students who have a totat late fee OF MORE than N")  
         print("3. Show the name of the publisher who's books were most borrowed")  
@@ -116,13 +145,13 @@ while True:
             p1.getMostBorrowedPublisher(conn)
 
         elif choice2 == 4:
-            pl.getAverageLateFee(conn) 
+            p1.getAverageLateFee(conn) 
 
         elif choice2 == 5:
-            pl.getTotalLateFee(conn)
+            p1.getTotalLateFee(conn)
             
         elif choice2 == 6:
-            pl.getCountForQuarter(conn)    
+            p1.getCountForQuarter(conn)    
             
         else:
             p1.close_connection(conn)
